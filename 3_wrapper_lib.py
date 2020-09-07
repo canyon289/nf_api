b59e0d79cfe63bc2031839007fd27fe552fba01d
Ravin Kumar <ravinsdrive@gmail.com>
2020-09-07 14:14:57 -0700

    Add broken api

---
+++ 3_wrapper_lib.py
@@ -0,0 +1,42 @@
+from github import Github
+import pandas as pd
+import re
+
+# Get a personal access token from github
+from p_token import personal_token
+
+pattern = "Review vote:([\s,a-z, :, +, - ]*)"
+
+# or using an access token
+g = Github(personal_token)
+
+repo = g.get_repo("OriolAbril/pymcon_reviews")
+# repo = g.get_repo("canyon289/nf_api")
+open_issues = repo.get_issues(state='open')
+
+
+
+rows = []
+
+for issue in open_issues:
+    d = {} 
+    d["title"] = issue.title
+    d["issue_number"] = issue.number
+
+    for comment in issue.get_comments():
+
+        if comment.user.login not in ("canyon289"):
+            d["reviewer"] = comment.user.login
+            d["body"] = comment.body 
+            vote = re.match(pattern, comment.body)
+
+            if vote is not None:
+                d["vote"] = vote[0]
+            else:
+                d["vote"] = vote
+            print(vote)
+
+            rows.append(d)
+
+df = pd.DataFrame(rows)
+df.to_excel("Reviews.xlsx")
